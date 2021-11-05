2c23e635a3430bd9ae73ba244edf94372885ea7c
Gábor Bóka <bokagabor4@gmail.com>
2021-11-05 17:11:29 +0100

    sharedPreferencesDone

--- main/java/com/example/tranzakcioelozmenyek/TranzactionManager.kt
+++ main/java/com/example/tranzakcioelozmenyek/TranzactionManager.kt
@@ -1,27 +1,53 @@
 package com.example.tranzakcioelozmenyek
 
 import android.content.Context
+import android.content.SharedPreferences
+import android.util.Log
 import android.widget.TextView
 import android.widget.Toast
 
 class TranzactionManager {
     companion object {
 
-        val SZALLAS_ID : Byte = 0
-        val VENDEGLATAS_ID : Byte = 1
-        val SZABADIDO_ID : Byte = 2
+        const val SZALLAS_ID : Int = 0
+        const val VENDEGLATAS_ID : Int = 1
+        const val SZABADIDO_ID : Int = 2
 
-        fun changeBalance(appContext: Context, balanceText : TextView?, isWithdrawing : Boolean, wId : Byte)
+        private var balances = mutableListOf(0u, 0u, 0u, 0u, 0u)
+
+        fun changeBalance(appContext: Context, amount: UInt, isWithdrawing : Boolean,  actID : Int, editor : SharedPreferences.Editor)
         {
-            Toast.makeText(appContext, "Működik", Toast.LENGTH_SHORT).show()
+            if(isWithdrawing)
+            {
+                balances[actID] -= amount
+                Toast.makeText(appContext, "Sikeres tranzakció", Toast.LENGTH_SHORT).show()
+            }
+            else
+            {
+                balances[actID] += amount
+                Toast.makeText(appContext, "Sikeres tranzakció", Toast.LENGTH_SHORT).show()
+            }
+            saveBalance(editor, balances[actID], actID)
         }
-        private fun saveBalance(ac : Context)
-        {
 
+        fun getBalance(id : Int) : UInt
+        {
+            return balances[id]
         }
-        private fun loadBalance(ac : Context)
-        {
 
+        private fun saveBalance(editor : SharedPreferences.Editor, amount : UInt, actID: Int)
+        {
+            editor.apply {
+                putInt(actID.toString(), amount.toInt())
+                apply()
+            }
+        }
+        fun loadBalance(sharedPreference : SharedPreferences, id : Int, txtView : TextView?)
+        {
+            val amount = sharedPreference.getInt(id.toString(), 0)
+            balances[id] = amount.toUInt()
+            val txt = "Egyenleg ${getBalance(id)} Ft"
+            txtView?.text = txt
         }
     }
 }
--- main/java/com/example/tranzakcioelozmenyek/Zsebek.kt
+++ main/java/com/example/tranzakcioelozmenyek/Zsebek.kt
@@ -2,100 +2,134 @@ package com.example.tranzakcioelozmenyek
 
 import android.content.Context
 import android.os.Bundle
-import android.text.Editable
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
+import android.content.SharedPreferences
+
+
+
+
 
 class Zsebek(context: Context) : Fragment(R.layout.fragment_zsebek) {
 
     private val appContext :Context = context
     private var isWithdrawing : Boolean = false
-    private var actID :Byte = -1
+    private var actID : Int = -1
+    private var txtBalances = mutableListOf<TextView?>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        
+
+        val sharedPreferences = this.requireActivity().getSharedPreferences("pref", Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+
+        txtBalances.add(getView()?.findViewById(R.id.szallasegyenleg))
+        txtBalances.add(getView()?.findViewById(R.id.vendeglatasegyenleg))
+        txtBalances.add(getView()?.findViewById(R.id.szabadidoegyenleg))
+
         val szAddBtn = getView()?.findViewById<Button>(R.id.addBtnSzallas)
         val szRmBtn = getView()?.findViewById<Button>(R.id.rmBtnSzallas)
-        val szallasBalance = getView()?.findViewById<TextView>(R.id.szallasegyenleg)
 
         val vAddBtn = getView()?.findViewById<Button>(R.id.addBtnVendeg)
         val vRmBtn = getView()?.findViewById<Button>(R.id.rmBtnVendeg)
-        val vendegBalance = getView()?.findViewById<TextView>(R.id.vendeglatasegyenleg)
 
         val szbAddBtn = getView()?.findViewById<Button>(R.id.addBtnSzabad)
         val szbRmBtn = getView()?.findViewById<Button>(R.id.rmBtnSzabad)
-        val szabadidoBalance = getView()?.findViewById<TextView>(R.id.szabadidoegyenleg)
 
-        val tr_window = getView()?.findViewById<ConstraintLayout>(R.id.tr_Window)
-        val trEt = tr_window?.findViewById<EditText>(R.id.trEt)
-        val tr_btnOk = tr_window?.findViewById<Button>(R.id.btn_Ok)
-        val tr_btnVissza = tr_window?.findViewById<Button>(R.id.btn_Vissza)
+        val trWindow = getView()?.findViewById<ConstraintLayout>(R.id.tr_Window)
+        val trEt = trWindow?.findViewById<EditText>(R.id.trEt)
+        val trWindowTxt = trWindow?.findViewById<TextView>(R.id.tr_windowText)
+        val trbtnOk = trWindow?.findViewById<Button>(R.id.btn_Ok)
+        val trbtnVissza = trWindow?.findViewById<Button>(R.id.btn_Vissza)
 
+        setListeners(szAddBtn, szRmBtn, vAddBtn, vRmBtn, szbAddBtn, szbRmBtn, trWindow, trbtnOk, trbtnVissza, trEt, trWindowTxt, editor)
+
+        init(sharedPreferences, txtBalances[0], txtBalances[1], txtBalances[2])
+
+    }
+
+    private fun init(sharedPreferences: SharedPreferences, szB: TextView?, vB: TextView?, szbB: TextView?)
+    {
+        TranzactionManager.loadBalance(sharedPreferences, TranzactionManager.SZALLAS_ID, szB)
+        TranzactionManager.loadBalance(sharedPreferences, TranzactionManager.VENDEGLATAS_ID, vB)
+        TranzactionManager.loadBalance(sharedPreferences, TranzactionManager.SZABADIDO_ID, szbB)
+    }
+
+    private fun setProperties(isW : Boolean, id: Int, tr_window: ConstraintLayout?, trwTxt: TextView?)
+    {
+        this.isWithdrawing = isW
+        this.actID = id
+        tr_window?.visibility = View.VISIBLE
+        trwTxt?.text = if(isW) getString(R.string.trw_with) else getString(R.string.trw_dep)
+    }
+
+    private fun setListeners(szAddBtn : Button? , szRmBtn : Button?, vAddBtn : Button?, vRmBtn : Button?, szbAddBtn: Button?, szbRmBtn : Button?
+    , tr_window : ConstraintLayout?, tr_btnOk: Button?, tr_btnVissza : Button?, trEt : EditText?
+    , trwTxt : TextView?, editor : SharedPreferences.Editor
+    )
+    {
         szAddBtn?.setOnClickListener {
-            setProperties(false, TranzactionManager.SZALLAS_ID)
-            if (tr_window != null) {
-                tr_window.visibility = View.VISIBLE
-            }
+            setProperties(false, TranzactionManager.SZALLAS_ID, tr_window, trwTxt)
         }
         szRmBtn?.setOnClickListener {
-            setProperties(true, TranzactionManager.SZALLAS_ID)
-            if (tr_window != null) {
-                tr_window.visibility = View.VISIBLE
-            }
+            setProperties(true, TranzactionManager.SZALLAS_ID, tr_window, trwTxt)
         }
 
         vAddBtn?.setOnClickListener {
-            setProperties(false, TranzactionManager.VENDEGLATAS_ID)
-            if (tr_window != null) {
-                tr_window.visibility = View.VISIBLE
-            }
+            setProperties(false, TranzactionManager.VENDEGLATAS_ID, tr_window, trwTxt)
         }
         vRmBtn?.setOnClickListener {
-            setProperties(true, TranzactionManager.VENDEGLATAS_ID)
-            if (tr_window != null) {
-                tr_window.visibility = View.VISIBLE
-            }
+            setProperties(true, TranzactionManager.VENDEGLATAS_ID, tr_window, trwTxt)
         }
 
         szbAddBtn?.setOnClickListener {
-            setProperties(false, TranzactionManager.SZABADIDO_ID)
-            if (tr_window != null) {
-                tr_window.visibility = View.VISIBLE
-            }
+            setProperties(false, TranzactionManager.SZABADIDO_ID, tr_window, trwTxt)
         }
         szbRmBtn?.setOnClickListener {
-            setProperties(true, TranzactionManager.SZABADIDO_ID)
-            if (tr_window != null) {
-                tr_window.visibility = View.VISIBLE
-            }
+            setProperties(true, TranzactionManager.SZABADIDO_ID, tr_window, trwTxt)
         }
 
 
         tr_btnVissza?.setOnClickListener {
             trEt?.text?.clear()
             tr_window?.visibility = View.INVISIBLE
+            closeKeyBoard()
         }
-
         tr_btnOk?.setOnClickListener {
-            if(trEt?.text.toString() == "")
+            val trValue = trEt?.text.toString()
+            if(trValue == "")
             {
                 Toast.makeText(appContext, "Nem adott meg összeget", Toast.LENGTH_SHORT).show()
             }
+            else {
+
+                if((trValue.toUInt() > TranzactionManager.getBalance(actID)) && isWithdrawing)
+                {
+                    Toast.makeText(appContext, "Ennyi pénz nem áll rendelkezésre", Toast.LENGTH_SHORT).show()
+                }
+                else
+                {
+                    TranzactionManager.changeBalance(appContext, trValue.toUInt(), isWithdrawing, actID, editor)
+                    trEt?.text?.clear()
+                    val txt = "Egyenleg: ${TranzactionManager.getBalance(actID)} Ft"
+                    txtBalances[actID]?.text = txt
+                    tr_window?.visibility = View.INVISIBLE
+                }
+            }
+            closeKeyBoard()
         }
     }
 
-    private fun setProperties(isW : Boolean, id: Byte)
+    private fun closeKeyBoard()
     {
-        this.isWithdrawing = isW
-        this.actID = id
-
+        val imm = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(requireView().windowToken, 0)
     }
 }
 
